b905a1151727482b34f2f75a88d0500ff5c79a78
Lion Kortlepel <development@kortlepel.com>
2019-09-10 14:46:56 +0200

    Initial commit
    Dumping my LK::math and LK::time here as a first addition to my personal library.

---
+++ LKmath.h
@@ -0,0 +1,43 @@
+// Copyright Lion Kortlepel 2019
+#ifndef LKMATH_H
+#define LKMATH_H
+
+#include <vector>
+#include <cmath>
+
+namespace LK
+{
+
+namespace math
+{
+
+template<typename _T>
+_T standard_deviation(const std::vector<_T>& vec, _T mean)
+{
+    std::vector<_T> others(vec.size());
+    for (std::size_t i = 0; i < vec.size(); ++i)
+    {
+        others[i] = (vec[i] - mean) * (vec[i] - mean);
+    }
+    _T others_sum = 0;
+    for (const auto& o : others)
+        others_sum += o;
+    return static_cast<_T>(sqrt(others_sum / others.size()));
+}
+
+template<typename _T>
+_T mean(const std::vector<_T>& vec)
+{
+    _T sum = 0;
+    for (const auto& t : vec)
+    {
+        sum += t; 
+    }
+    return sum / vec.size();
+}
+
+}
+
+}
+
+#endif // LKMATH_H
---
+++ LKtime.h
@@ -0,0 +1,48 @@
+#ifndef LKTIME_H
+#define LKTIME_H
+
+#include <vector>
+#include <chrono>
+#include <utility>
+#include "LKmath.h"
+
+namespace LK
+{
+
+namespace time
+{
+
+/// @name time_me
+/// @brief Measures a function's execution time 'n' times and returns mean and standard 
+///     deviation in milliseconds. Measuring occurs in nanosecond resolution.
+template<typename _Fnc, typename ..._Args>
+std::pair<double, double> time_me(_Fnc fnc, std::size_t n, _Args ...args)
+{
+    using namespace std::chrono;
+    std::vector<double> times(n);
+    
+    for (std::size_t i = 0; i < n; ++i)
+    {
+        auto start = high_resolution_clock::now();
+        
+        fnc(args...);
+        
+        times.at(i) = (high_resolution_clock::now() - start).count() / 1000000.0;
+#ifdef LK_DEBUG_TIME_ME_FN
+        printf("%.10g\n", times.at(i));
+#endif
+    }
+    
+    double mean = LK::math::mean(times);
+    double std_dev = LK::math::standard_deviation(times, mean);
+    
+    //printf("Result:\n  Mean: %.10gms\n  Standard Deviation: %.10gms\n", mean, std_dev);
+    return { mean, std_dev };
+}
+
+}
+
+}
+
+
+#endif // LKTIME_H
